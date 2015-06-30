a842f06a21ee7c7f7eb8a0ab6866a23661a5aebe
Yoda <qyfbq@163.com>
2015-06-30 10:41:17 +0800

    init

---
+++ index.js
@@ -0,0 +1,65 @@
+var express = require('express');
+var webot = require('weixin-robot');
+
+var app = express();
+
+app.get("/mytest", function(req, res, next) {
+	res.end("123");
+});
+
+// 指定回复消息
+webot.set('hi', '你好');
+
+webot.set('subscribe', {
+  pattern: function(info) {
+    return info.is('event') && info.param.event === 'subscribe';
+  },
+  handler: function(info) {
+    return '欢迎订阅微信机器人';
+  }
+});
+
+webot.set('test', {
+  pattern: /^test/i,
+  handler: function(info, next) {
+    next(null, 'roger that!')
+  }
+})
+
+// 你可以获取已定义的 rule
+//
+// webot.get('subscribe') ->
+//
+// {
+//   name: 'subscribe',
+//   pattern: function(info) {
+//     return info.is('event') && info.param.event === 'subscribe';
+//   },
+//   handler: function(info) {
+//     return '欢迎订阅微信机器人';
+//   }
+// }
+//
+
+// 接管消息请求
+webot.watch(app, { token: 'your1weixin2token', path: '/wechat' });
+
+// 如果需要多个实例（即为多个微信账号提供不同回复）：
+var webot2 = new webot.Webot();
+webot2.set({
+  '/hi/i': 'Hello',
+  '/who (are|r) (you|u)/i': 'I\'m a robot.'
+});
+webot2.watch(app, {
+  token: 'token2',
+  path: '/wechat_en', // 这个path不能为之前已经监听过的path的子目录
+});
+
+// 启动 Web 服务
+// 微信后台只允许 80 端口
+app.listen(80);
+
+// 如果你不想让 node 应用直接监听 80 端口
+// 可以尝试用 nginx 或 apache 自己做一层 proxy
+// app.listen(process.env.PORT);
+// app.enable('trust proxy');
